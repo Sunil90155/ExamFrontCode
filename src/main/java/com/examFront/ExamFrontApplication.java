3d789c7c71174fcce668957b69c772eecd832014
Sunil <sunil.90155@gmail.com>
2022-10-26 20:18:49 +0530

    First Time with Hi

---
+++ src/main/java/com/examFront/ExamFrontApplication.java
@@ -0,0 +1,15 @@
+package com.examFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamFrontApplication.class, args);
+
+		System.out.printf("Hi");
+	}
+
+}
---
+++ src/test/java/com/examFront/ExamFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
